8ddda5c837db4897c36e0c741c469e200a490fd7
xovest <79008917+xovest@users.noreply.github.com>
2021-12-03 18:15:48 +0200

    nice stylez

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,14 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-import Navbar from '../components/Navbar'
-import Footer from '../components/Footer'
 import Link from 'next/link'
+import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
     <div>
-      <h1>yo</h1>
-      <p>This is something that should be a paragraph so im just adding words just for the record</p>
-      <p>This is another something that should be a paragraph so im just adding words just for the record</p>
+      <h1 className={styles.title}>yo</h1>
+      <p className={styles.text}>This is something that should be a paragraph so im just adding words just for the record</p>
+      <p className={styles.text}>This is another something that should be a paragraph so im just adding words just for the record</p>
       <Link href="/people">
-        <a>See Ppl Listing</a>
+        <a className={styles.btn}>See Ppl Listing</a>
       </Link>
     </div>
   )
