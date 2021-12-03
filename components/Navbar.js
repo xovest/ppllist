708a34147a5e24e6336e30ba984c17aa624332ae
xovest <79008917+xovest@users.noreply.github.com>
2021-12-03 17:59:21 +0200

    link

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,12 +1,14 @@
+import Link from 'next/link'
+
 const Navbar = () => {
   return (
     <nav>
       <div className="logo">
         <h1>People List</h1>
       </div>
-      <a>Home</a>
-      <a>About</a>
-      <a>People Listing</a>
+      <Link href="/"><a>Home</a></Link>
+      <Link href="/about"><a>About</a></Link>
+      <Link href="/people"><a>Person Listing</a></Link>
     </nav>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
+import Link from 'next/link'
 
 export default function Home() {
   return (
@@ -11,6 +12,9 @@ export default function Home() {
       <h1>yo</h1>
       <p>This is something that should be a paragraph so im just adding words just for the record</p>
       <p>This is another something that should be a paragraph so im just adding words just for the record</p>
+      <Link href="/people">
+        <a>See Ppl Listing</a>
+      </Link>
       <Footer />
     </div>
   )
