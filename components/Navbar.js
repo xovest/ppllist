41f35e408f84efc3aa4cc446ef9dfb0f9d0019d7
xovest <79008917+xovest@users.noreply.github.com>
2021-12-03 17:50:40 +0200

    made components

---
+++ components/Navbar.js
@@ -0,0 +1,14 @@
+const Navbar = () => {
+  return (
+    <nav>
+      <div className="logo">
+        <h1>People List</h1>
+      </div>
+      <a>Home</a>
+      <a>About</a>
+      <a>People Listing</a>
+    </nav>
+  );
+}
+ 
+export default Navbar;
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,17 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
 
 export default function Home() {
   return (
     <div>
+      <Navbar />
       <h1>yo</h1>
       <p>This is something that should be a paragraph so im just adding words just for the record</p>
       <p>This is another something that should be a paragraph so im just adding words just for the record</p>
+      <Footer />
     </div>
   )
 }
