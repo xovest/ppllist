9be1fb56d2f9a7b6ad91663f892c62990b57135e
xovest <79008917+xovest@users.noreply.github.com>
2021-12-03 19:25:38 +0200

    data = fetched

--- pages/people/index.js
+++ pages/people/index.js
@@ -1,16 +1,28 @@
-import Head from 'next/head'
+import styles from '../../styles/Ppl.module.css'
 
-const People = () => {
+export const getStaticProps = async () => {
+  const res = await fetch('https://jsonplaceholder.typicode.com/users');
+  const data = await res.json();
+
+  return {
+    props: {
+      ppl: data
+    }
+  };
+}
+
+const People = ({ ppl }) => {
   return (
-    <>
-      <Head>
-        <title>Ppl List | People</title>
-        <meta name="keywords" content="people" />
-      </Head>
-      <div>
-        <h1>All them ppl</h1>
-      </div>
-    </>
+    <div>
+      <h1>All them ppl</h1>
+      { ppl.map(person => (
+        <div key={person.id}>
+          <a className={styles.single}>
+            <h3>{ person.name }</h3>
+          </a>
+        </div>
+      ))}
+    </div>
   );
 }
  
